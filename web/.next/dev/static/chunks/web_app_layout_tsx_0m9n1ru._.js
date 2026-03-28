787b34f986dc921c9b307065bbf652d23810e70a
Sourav Mane <souravmane3000@example.com>
2026-03-28 18:37:04 +0530

    add web app and gitignore

---
+++ web/.next/dev/static/chunks/web_app_layout_tsx_0m9n1ru._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__066bv.2._.css","static/chunks/063q_next_dist_0jszkyh._.js"],
+    source: "dynamic"
+});
